baa30dd9bf8c07ff85826d4e0554512638b1deec
Jota-ato <julioc-01@outlook.com>
2026-06-03 13:13:20 -0600

    feat: add better-auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,16 @@
 import { community } from "@/db/schema/community"
+import { users, sessions, accounts, verifications, usersRelations, sessionsRelations, accountsRelations } from "@/db/schema/auth-schema"
 import { drizzle } from 'drizzle-orm/node-postgres'
 
-export const db = drizzle("postgresql://neondb_owner:npg_5kPDcQCL1mlF@ep-young-hat-aq2t7hmt-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require", {
+export const db = drizzle(process.env.DATABASE_URL!, {
     schema: {
-        community
+        community,
+        users,
+        sessions,
+        accounts,
+        verifications,
+        usersRelations,
+        sessionsRelations,
+        accountsRelations,
     }
-}) 
+})
