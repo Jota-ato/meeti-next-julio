57927e1ee30b6ecefecfdfe8a87add81d7a96ff9
Jota-ato <julioc-01@outlook.com>
2026-06-03 14:20:30 -0600

    fix: solve zod incompatibilitie

--- src/features/auth/schemas/authSchema.ts
+++ src/features/auth/schemas/authSchema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 const BaseAuthSchema = z.object({
-    name: z.string().trim().min(1, { error: 'El nombre es obligatorio' }),
-    email: z.email({ error: 'E-mail no válido' }),
+    name: z.string().trim().min(1, { message: 'El nombre es obligatorio' }),
+    email: z.string().email({ message: 'E-mail no válido' }),
     password: z.string()
         .trim()
-        .min(8, { error: 'Mínimo 8 caracteres en la contraseña' })
-        .regex(/[A-Z]/, { error: 'Debe contener al menos una mayúscula' })
-        .regex(/[^a-zA-Z0-9]/, { error: 'Debe contener al menos un carácter especial' }),
+        .min(8, { message: 'Mínimo 8 caracteres en la contraseña' })
+        .regex(/[A-Z]/, { message: 'Debe contener al menos una mayúscula' })
+        .regex(/[^a-zA-Z0-9]/, { message: 'Debe contener al menos un carácter especial' }),
 })
 
 export const SignInSchema = BaseAuthSchema.pick({
@@ -18,11 +18,11 @@ export const SignInSchema = BaseAuthSchema.pick({
 export type SignInType = z.infer<typeof SignInSchema>
 
 export const SignUpSchema = BaseAuthSchema.extend({
-    passwordConfirmation: z.string().min(1, { error: 'Confirma tu contraseña' })
+    passwordConfirmation: z.string().min(1, { message: 'Confirma tu contraseña' })
 }).refine(
     (data) => data.password === data.passwordConfirmation,
     {
-        error: 'Las contraseñas no coinciden',
+        message: 'Las contraseñas no coinciden',
         path: ['passwordConfirmation']
     }
 )
