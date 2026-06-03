78ef49106c12d8ac44ae075bff347d43e3c54292
Jota-ato <julioc-01@outlook.com>
2026-06-03 10:33:26 -0600

    force database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     schema: './src/db/schema',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!
+        url: "postgresql://neondb_owner:npg_5kPDcQCL1mlF@ep-young-hat-aq2t7hmt-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"!
     }
 })
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { community } from "@/db/schema/community"
 import { drizzle } from 'drizzle-orm/node-postgres'
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+export const db = drizzle("postgresql://neondb_owner:npg_5kPDcQCL1mlF@ep-young-hat-aq2t7hmt-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require", {
     schema: {
         community
     }
