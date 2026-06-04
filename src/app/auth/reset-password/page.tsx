fd1f8b7c8483e9b23e77162c1e0df46d4304ea09
Jota-ato <julioc-01@outlook.com>
2026-06-04 11:32:43 -0600

    fix: add suspense to solve url error

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,8 +1,8 @@
-import ForgotPasswordForm from "@/features/auth/components/forgot-password-form";
 import { SetPasswordForm } from "@/features/auth/components/set-password-form";
 import { Heading } from "@/shared/components/typography/heading";
 import { Metadata } from "next";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: 'Definir nueva contraseña'
@@ -14,7 +14,11 @@ export default function ResetPasswordPage() {
             <Heading className="text-center mb-8">
                 Definir nuevo password
             </Heading>
-            <SetPasswordForm />
+
+            <Suspense fallback={<p className="text-center text-sm text-muted-foreground">Cargando formulario...</p>}>
+                <SetPasswordForm />
+            </Suspense>
+            
             <nav className="mt-4 px-4 flex justify-between font-bold">
                 <Link
                     href="/auth/sign-in"
