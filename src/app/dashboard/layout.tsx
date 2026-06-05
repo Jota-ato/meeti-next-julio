3cef1c2d39524afcaa30045575968023707fb15a
Jota-ato <julioc-01@outlook.com>
2026-06-04 19:19:41 -0600

    feat: add proxy to protect routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
+import { auth } from "@/lib/auth";
 import { DashboardSidebar } from "@/shared/components/dashboard/sidebar/dashboard-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/shared/components/ui/sidebar";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export default async function DashboardLayout({
@@ -7,6 +10,15 @@ export default async function DashboardLayout({
 }: {
     children: ReactNode
 }) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        redirect("/auth/sign-in")
+    }
+
     return (
         <>
             <SidebarProvider>
